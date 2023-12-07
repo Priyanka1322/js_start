13205e5d830a2af3a69a8b1caedcef03c0361f51
Priyanka Mumbarkar <98268948+Priyanka1322@users.noreply.github.com>
2023-12-07 07:55:13 +0000

    array and obj

--- js_code/array.js
+++
@@ -1,2 +0,0 @@
-Object.myArr = [0,1,2,1,2,]
-console.log(myArr[0,1]);
---
+++ js_code/obj.js
@@ -0,0 +1,12 @@
+const tinderUser = {}
+tinderUser.id ="122@ads"
+tinderUser.name = "sam"
+tinderUser.isLogIn = false
+
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(Object.values(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty(isLogIn));
